5a468199b7369eb4e546889c4e52500608fd5f09
Ravikachhadiya <ravikachhadiya@gmail.com>
2020-09-11 18:31:31 +0530

    session 8.10

---
+++ lib/providers/products.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/cupertino.dart';
+import '../models/product.dart';
+
+class Products with ChangeNotifier {
+  List<Product> _items = [
+    Product(
+      id: 'p1',
+      title: 'Red Shirt',
+      description: 'A red shirt - it is pretty red!',
+      price: 29.99,
+      imageUrl:
+          'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
+    ),
+    Product(
+        id: 'p2',
+        title: 'Trousers',
+        description: 'A nice pair of trousers.',
+        price: 59.99,
+        imageUrl:
+            'https://images-na.ssl-images-amazon.com/images/I/81t1RJtrHDL._UL1500_.jpg'),
+  ];
+
+  List<Product> get items {
+    return [..._items];
+  }
+
+  void addProduct() {
+    // _items.add(value);
+    notifyListeners();
+  }
+}
