308236612d0a4017a8186630f22abae4d9ce39a4
Ravikachhadiya <ravikachhadiya@gmail.com>
2020-10-04 18:24:15 +0530

    session 12.7

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -23,9 +23,12 @@ class ProductDetailScreen extends StatelessWidget {
             Container(
               height: 300,
               width: double.infinity,
-              child: Image.network(
-                loadedProduct.imageUrl,
-                fit: BoxFit.cover,
+              child: Hero(
+                tag: loadedProduct.id,
+                child: Image.network(
+                  loadedProduct.imageUrl,
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
             SizedBox(
@@ -38,9 +41,17 @@ class ProductDetailScreen extends StatelessWidget {
                 fontSize: 20,
               ),
             ),
-            SizedBox(height: 10,),
-            Container(padding: EdgeInsets.symmetric(horizontal: 10),
-              width: double.infinity, child: Text(loadedProduct.description, textAlign: TextAlign.center, softWrap: true,)),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                width: double.infinity,
+                child: Text(
+                  loadedProduct.description,
+                  textAlign: TextAlign.center,
+                  softWrap: true,
+                )),
           ],
         ),
       ),
